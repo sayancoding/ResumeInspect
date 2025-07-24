ab28febfbdcc70208dd0a91e10e1222cdc8659cf
sayancoding <sayanmaity007@gmail.com>
2025-07-24 16:20:57 +0530

    Update prompts and Agent

--- crews/basic_crew.py
+++ crews/basic_crew.py
@@ -1,10 +1,10 @@
 from crewai import Crew
 
 from agents.resume_query_agent import resume_query_agent
-from tasks.question_task import question_task
+from tasks.resume_summary_task import resume_summary_task
 
 basic_crew = Crew(
     agents=[resume_query_agent],
-    tasks=[question_task],
+    tasks=[resume_summary_task],
     verbose=True
 )
